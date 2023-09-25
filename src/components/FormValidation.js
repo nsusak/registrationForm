be8f02a0400e70f1fa723e512048c98d77ff29c3
nsusak <nikolina@travelraysoftware.com>
2023-09-25 10:04:11 +0200

    languages

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -9,6 +9,78 @@ const FormValidation = () => {
     formState: { errors },
   } = useForm();
   const [step, setStep] = useState(1);
+  const [language, setLanguage] = useState("english");
+  const languageLabels = {
+    english: {
+      english: "English",
+      spanish: "Spanish",
+      firstName: "First Name",
+      lastName: "Last Name",
+      username: "Username",
+      email: "Email",
+      password: "Password",
+      confirmPassword: "Confirm Password",
+      termsAndConditions: "I accept the terms and conditions",
+      nextButton: "Next",
+      submitButton: "Submit",
+      errorMessages: {
+        firstNameRequired: "First name is required",
+        firstNamePattern: "Alphabetical characters only",
+        firstNameLength:
+          "Name must be greater than 2 characters and less than 25",
+        lastNameRequired: "Last name is required",
+        lastNamePattern: "Alphabetical characters only",
+        lastNameLength:
+          "Name must be greater than 2 characters and less than 25",
+        usernameRequired: "Username is required",
+        usernameMaxLength: "Username should be less than 20 characters",
+        emailRequired: "Email is required",
+        emailPattern: "Please write correct email address",
+        passwordRequired: "Password is required",
+        passwordMinLength: "Your password must be greater than 8 characters",
+        confirmPasswordRequired: "Please confirm your password",
+        confirmPasswordValidate: "Passwords do not match",
+        termsAndConditionsRequired: "Please accept the terms and conditions",
+      },
+    },
+    spanish: {
+      english: "Inglés",
+      spanish: "Español",
+      firstName: "Nombre",
+      lastName: "Apellido",
+      username: "Nombre de usuario",
+      email: "Correo electrónico",
+      password: "Contraseña",
+      confirmPassword: "Confirmar contraseña",
+      termsAndConditions: "Acepto los términos y condiciones",
+      nextButton: "Siguiente",
+      submitButton: "Enviar",
+      errorMessages: {
+        firstNameRequired: "Nombre es obligatorio",
+        firstNamePattern: "Solo caracteres alfabéticos",
+        firstNameLength:
+          "El nombre debe tener más de 2 caracteres y menos de 25",
+        lastNameRequired: "Apellido es obligatorio",
+        lastNamePattern: "Solo caracteres alfabéticos",
+        lastNameLength:
+          "El apellido debe tener más de 2 caracteres y menos de 25",
+        usernameRequired: "Nombre de usuario es obligatorio",
+        usernameMaxLength:
+          "El nombre de usuario debe tener menos de 20 caracteres",
+        emailRequired: "Correo electrónico es obligatorio",
+        emailPattern:
+          "Por favor, escriba una dirección de correo electrónico válida",
+        passwordRequired: "Contraseña es obligatoria",
+        passwordMinLength: "Su contraseña debe tener más de 8 caracteres",
+        confirmPasswordRequired: "Por favor, confirme su contraseña",
+        confirmPasswordValidate: "Las contraseñas no coinciden",
+        termsAndConditionsRequired:
+          "Por favor, acepte los términos y condiciones",
+      },
+    },
+  };
+
+  const labels = languageLabels[language];
 
   const onNextStep = async () => {
     if (step === 1) {
@@ -25,25 +97,27 @@ const FormValidation = () => {
         console.log(watch(data));
       })();
     }
-
-    // console.log(isStepValid, "is step valid");
   };
-  //   const onSubmit = (data, event) => {
-  //     console.log("TRYING OT SUBMIT");
-  //     console.log("Form submitted with data:", data);
-  //     // if (step === 1) {
-  //     //   setStep(2);
-  //     //   event.preventDefault();
-  //     // } else if (step === 2) {
-  //     //   event.preventDefault();
-  //     //   alert(JSON.stringify(data));
-  //     //   console.log(watch(data));
-  //     // }
-  //     console.log(event, data);
-  //   };
 
   return (
     <div>
+      <div>
+        <div class="inline-flex mt-2">
+          <button
+            onClick={() => setLanguage("english")}
+            class="bg-white hover:bg-orange-400 text-orange-800 font-bold py-2 px-4 rounded-l border"
+          >
+            {labels.english}
+          </button>
+
+          <button
+            onClick={() => setLanguage("spanish")}
+            class="bg-white hover:bg-orange-400 text-orange-800 font-bold py-2 px-4 rounded-r border"
+          >
+            {labels.spanish}
+          </button>
+        </div>
+      </div>
       <div className="w-96 m-40 text-left">
         <form
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
@@ -52,7 +126,7 @@ const FormValidation = () => {
           {step === 1 && (
             <div>
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                First Name
+                {labels.firstName}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -68,21 +142,25 @@ const FormValidation = () => {
               />
 
               {errors?.firstName?.type === "required" && (
-                <p className="text-red-500">First name is required</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.firstNameRequired}
+                </p>
               )}
               {errors?.firstName?.type === "pattern" && (
-                <p className="text-red-500">Alphabetical characters only</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.firstNamePattern}
+                </p>
               )}
 
               {(errors?.firstName?.type === "minLength" ||
                 errors?.firstName?.type === "maxLength") && (
                 <p className="text-red-500">
-                  Name must be greater than 2 characters and less than 25
+                  {labels.errorMessages.firstNameLength}
                 </p>
               )}
 
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                Last Name
+                {labels.lastName}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -100,15 +178,19 @@ const FormValidation = () => {
               {(errors?.lastName?.type === "minLength" ||
                 errors?.lastName?.type === "maxLength") && (
                 <p className="text-red-500">
-                  Name must be greater than 2 characters and less than 25
+                  {labels.errorMessages.lastNameLength}
                 </p>
               )}
 
               {errors?.lastName?.type === "required" && (
-                <p className="text-red-500">Last name is required</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.lastNameRequired}
+                </p>
               )}
               {errors?.lastName?.type === "pattern" && (
-                <p className="text-red-500">Alphabetical characters only</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.lastNamePattern}
+                </p>
               )}
               {errors && (
                 <button
@@ -116,7 +198,7 @@ const FormValidation = () => {
                   onClick={onNextStep}
                   className="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3"
                 >
-                  Next
+                  {labels.nextButton}
                 </button>
               )}
             </div>
@@ -125,7 +207,7 @@ const FormValidation = () => {
           {step === 2 && (
             <div>
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                Username
+                {labels.username}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -139,16 +221,18 @@ const FormValidation = () => {
               />
 
               {errors?.username?.type === "required" && (
-                <p className="text-red-500">Username is required</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.usernameRequired}
+                </p>
               )}
               {errors?.username?.type === "maxLength" && (
                 <p className="text-red-500">
-                  Username should be less than 20 characters
+                  {labels.errorMessages.usernameMaxLength}
                 </p>
               )}
 
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                Email
+                {labels.email}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -163,16 +247,18 @@ const FormValidation = () => {
               />
 
               {errors?.email?.type === "required" && (
-                <p className="text-red-500">Email is required</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.emailRequired}
+                </p>
               )}
               {errors?.email?.type === "pattern" && (
                 <p className="text-red-500">
-                  Please write correct email address
+                  {labels.errorMessages.emailPattern}
                 </p>
               )}
 
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                Password
+                {labels.password}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -189,11 +275,13 @@ const FormValidation = () => {
               />
 
               {errors?.password?.type === "required" && (
-                <p className="text-red-500">Password is required</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.passwordRequired}
+                </p>
               )}
               {errors?.password?.type === "minLength" && (
                 <p className="text-red-500">
-                  Your password must be greater than 8 characters
+                  {labels.errorMessages.passwordMinLength}
                 </p>
               )}
 
@@ -204,7 +292,7 @@ const FormValidation = () => {
               )} */}
 
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
-                Confirm Password
+                {labels.confirmPassword}
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -218,10 +306,14 @@ const FormValidation = () => {
               />
 
               {errors?.passwordConfirm?.type === "required" && (
-                <p className="text-red-500">Please confirm your password</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.confirmPasswordRequired}
+                </p>
               )}
               {errors?.passwordConfirm?.type === "validate" && (
-                <p className="text-red-500">Passwords do not match</p>
+                <p className="text-red-500">
+                  {labels.errorMessages.confirmPasswordValidate}
+                </p>
               )}
 
               <label className="block text-gray-700 text-sm font-bold mt-3 mb-2">
@@ -231,12 +323,12 @@ const FormValidation = () => {
                     required: true,
                   })}
                 />
-                I accept the terms and conditions
+                {labels.termsAndConditions}
               </label>
 
               {errors?.termsAndConditions?.type === "required" && (
                 <p className="text-red-500">
-                  Please accept the terms and conditions
+                  {labels.errorMessages.termsAndConditionsRequired}
                 </p>
               )}
 
@@ -245,7 +337,7 @@ const FormValidation = () => {
                 onClick={onNextStep}
                 className="w-full bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-3"
               >
-                Submit
+                {labels.submitButton}
               </button>
             </div>
           )}
